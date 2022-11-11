39e03cbff866ec3b230793fc12a953662fc5d470
Shawna White <sw1056@gmail.com>
2022-11-11 14:07:40 -0600

    Added remaining user prompts.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 inquirer
     .prompt([
@@ -17,5 +18,36 @@ inquirer
             type: 'input',
             message: 'Please provide installation instructions for the project.',
             name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Please provide usage instructions for the project.',
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: 'Please select a license for the project.',
+            name: 'license',
+            Choices: ["MIT", "ISC", "Apache", "GPL", "BSD", "GNU", "Mozilla-Public", "None"]
+        },
+        {
+            type: 'input',
+            message: 'Please provide contribution guidelines for the project.',
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: 'Please provide instructions for how to test the project.',
+            name: 'tests'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username.',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address.',
+            name: 'email'
         },
     ]);
