8c3f91a19ac8eda2797f03e17c5bbbee7d844da9
Shawna White <sw1056@gmail.com>
2022-11-11 15:38:29 -0600

    added generateMarkdown.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Global requires
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 //User input array
 const questions = [
