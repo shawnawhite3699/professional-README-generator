ecd1128d4a6754c99eaf37de18f163214f547115
Shawna White <sw1056@gmail.com>
2022-11-11 15:23:11 -0600

    Realized I was supposed to use starter code! Made minor changes based on that. Added write, initialize, and call functions. Added comments.

--- index.js
+++ index.js
@@ -1,117 +1,138 @@
+//Global requires
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { default: Choices } = require('inquirer/lib/objects/choices');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of this project?',
-            name: 'title',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a project title to continue.');
-                    return false;
-                }
+//User input array
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of this project?',
+        name: 'title',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title to continue.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            message: 'Please provide a description of the project.',
-            name: 'description',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a project description to continue.');
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description of the project.',
+        name: 'description',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description to continue.');
                     return false;
-                }
             }
-        },
-        {
-            type: 'input',
-            message: 'Please provide installation instructions for the project.',
-            name: 'installation',
-            validate: installationInput => {
-                if (installationInput) {
-                    return true;
-                } else {
-                    console.log('Please enter project installation instructions (or N/A) to continue.');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide installation instructions for the project.',
+        name: 'installation',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter project installation instructions (or N/A) to continue.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            message: 'Please provide usage instructions for the project.',
-            name: 'usage',
-            validate: usageInput => {
-                if (usageInput) {
-                    return true;
-                } else {
-                    console.log('Please describe how to use the project to continue.');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide usage instructions for the project.',
+        name: 'usage',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please describe how to use the project to continue.');
+                return false;
             }
-        },
-        {
-            type: 'list',
-            message: 'Please select a license for the project.',
-            name: 'license',
-            Choices: ["MIT", "ISC", "Apache", "GPL", "BSD", "GNU", "Mozilla-Public", "None"],
-            validate: licenseInput => {
-                if (licenseInput) {
-                    return true;
-                } else {
-                    console.log('Please select a license from the list to continue.');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'list',
+        message: 'Please select a license for the project.',
+        name: 'license',
+        choices: ["MIT", "ISC", "Apache", "GPL", "BSD", "GNU", "Mozilla-Public", "None"],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please select a license from the list to continue.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            message: 'Please provide contribution guidelines for the project.',
-            name: 'contributing',
-            validate: contributingInput => {
-                if (contributingInput) {
-                    return true;
-                } else {
-                    console.log('Please describe how others can contribute to the project to continue.');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide contribution guidelines for the project.',
+        name: 'contributing',
+        validate: contributingInput => {
+            if (contributingInput) {
+                return true;
+            } else {
+                console.log('Please describe how others can contribute to the project to continue.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            message: 'Please provide instructions on how to test the project.',
-            name: 'tests',
-            validate: testsInput => {
-                if (testsInput) {
-                    return true;
-                } else {
-                    console.log('Please explain how to test the project to continue.');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide instructions on how to test the project.',
+        name: 'tests',
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Please explain how to test the project to continue.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            message: 'Please enter your GitHub username.',
-            name: 'github',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username to continue.');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username.',
+        name: 'github',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username to continue.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            message: 'Please enter your email address.',
-            name: 'email'
-        },
-    ]);
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address.',
+        name: 'email'
+    },
+];
+
+//Write README function
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Information successfully transferred to the README!')
+    });
+};
+    
+//Initialize app function
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
+
+//Function call to initialize app
+init();
